a03c723eaa7e7e6885ec93432458838c3bcbddce
Balaganesh S <balaganesh@juspay.in>
2025-11-21 15:02:23 +0530

    Updated Package for version 2.2.5

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "HyperQR",
-            url: "https://public.releases.juspay.in/release/ios/hyper-sdk/2.2.4.17/HyperQR.zip",
-            checksum: "4a23ef6d4f4684e69eab5314dec274af6758a656c0592c084cbc8e8ead20b1e6"
+            url: "https://public.releases.juspay.in/release/ios/hyper-sdk/2.2.5/HyperQR.zip",
+            checksum: "7426918f258a4a55edbc57cc14d2952c9367e7c9a77ae039e2be3ddaf15838f1"
         )
     ]
 )
